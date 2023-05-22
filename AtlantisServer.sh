30956fbc501329803811c6d6861ce1aebe6a3bc6
AbdElRahman Yousry <ar.yousry@gmail.com>
2023-05-22 11:26:19 +0300

    Shipped Build Test 1

--- AtlantisServer.sh
+++ AtlantisServer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/Atlantis/Binaries/Linux/AtlantisServer"
-"$UE4_PROJECT_ROOT/Atlantis/Binaries/Linux/AtlantisServer" Atlantis "$@" 
+chmod +x "$UE4_PROJECT_ROOT/Atlantis/Binaries/Linux/AtlantisServer-Linux-Shipping"
+"$UE4_PROJECT_ROOT/Atlantis/Binaries/Linux/AtlantisServer-Linux-Shipping" Atlantis "$@" 
